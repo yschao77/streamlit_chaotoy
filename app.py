cf547e930caeeb5a52c32f5e0666c4e349acfd50
yschao77 <yschao77@gmail.com>
2026-06-26 14:42:22 +0800

    Comment out GDrive upload function in app.py
    Comment out the upload function for the inward file.

--- app.py
+++ app.py
@@ -853,7 +853,7 @@ elif sub_page == "🔀 sitegiant 採購入庫單格式轉換":
             
         final_filename = f"sitegiant採購入庫單_{current_vendor}_{current_order}.xlsx"
 
-        upload_or_update_gdrive_file(ID_HISTORY_INWARD_FOLDER, final_filename, towrite_inward.getvalue())
+       # upload_or_update_gdrive_file(ID_HISTORY_INWARD_FOLDER, final_filename, towrite_inward.getvalue())
         st.download_button(label=f"📥 下載sitegiant格式採購入庫單 ({final_filename})", data=towrite_inward.getvalue(), file_name=final_filename, mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", type="primary")
 
 # -------------------------------------------------------------------------
